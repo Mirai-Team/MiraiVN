7b2589b5cdf2e0132ce0693165a30391915100b6
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-02-16 19:30:48 +0100

    Corrected some mistakes in cmake macros.

--- cmake/macros/clang-settings.cmake
+++ cmake/macros/clang-settings.cmake
@@ -3,7 +3,7 @@ message(STATUS "Found CLang ")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 message(STATUS "CLang: Enabled c++11 support")
 
-if(MIRAI_WITH_WARNINGS)
+if(VNE_WITH_WARNINGS)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -pedantic-errors -Wall -Wextra -Wold-style-cast -Woverloaded-virtual -Wfloat-equal -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wconversion -Wshadow -Wredundant-decls -Winit-self -Wswitch-default -Wundef -Winline -Weffc++")
     message(STATUS "CLang: Enabled warnings")
 endif()
