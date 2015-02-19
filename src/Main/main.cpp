c5dc8a887dd3ae3362c19b10e39f12c7405423f0
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-02-19 13:18:07 +0100

    Renamed some vars

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -44,17 +44,17 @@ int main()
     sf::Clock clock;
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
 
-    mp::WindowManager mainWindowManager;
-    sf::RenderWindow &window = mainWindowManager.getWindow();
+    mp::WindowManager windowManager;
+    sf::RenderWindow &window = windowManager.getWindow();
 
-    mainWindowManager.setOptimalWinWidth(1280);
-    mainWindowManager.setOptimalWinHeight(1024);
-    mainWindowManager.setVideomodeWidth(1280);
-    mainWindowManager.setVideomodeHeight(1024);
-    mainWindowManager.setCursorVisible(true);
-    mainWindowManager.setFullscreen(false);
-    mainWindowManager.drawBorders();
-    mainWindowManager.create();
+    windowManager.setOptimalWinWidth(1280);
+    windowManager.setOptimalWinHeight(1024);
+    windowManager.setVideomodeWidth(1280);
+    windowManager.setVideomodeHeight(1024);
+    windowManager.setCursorVisible(true);
+    windowManager.setFullscreen(false);
+    windowManager.drawBorders();
+    windowManager.create();
 
     mvn::Character *lomadriel = new mvn::Character("Lomadriel", sf::Color::Red);
     mvn::Character *enerto = new mvn::Character("Enerto", sf::Color::Green);
@@ -66,9 +66,9 @@ int main()
         return EXIT_FAILURE;
     sf::Sprite sprite(texture);
 
-    mvn::DialogueSkin skin(sprite, sf::Vector2f(0.f, static_cast<float>(mainWindowManager.getVideomodeHeight()) -200.f), "resources/fonts/UbuntuMono-R.ttf", 20, sf::Color::White, sf::Vector2f(5.f, -22.f), sf::Vector2f(15.f, 20.f));
-    mvn::DialogueFrame mainDialogFrame(skin);
-    mvn::DialogueManager dialogueManager(mainDialogFrame, 50, sf::Time::Zero, true);
+    mvn::DialogueSkin skin(sprite, sf::Vector2f(0.f, static_cast<float>(windowManager.getVideomodeHeight()) -200.f), "resources/fonts/UbuntuMono-R.ttf", 20, sf::Color::White, sf::Vector2f(5.f, -22.f), sf::Vector2f(15.f, 20.f));
+    mvn::DialogueFrame dialogFrame(skin);
+    mvn::DialogueManager dialogueManager(dialogFrame, 50, sf::Time::Zero, true);
 
     dialogueManager.addDialogue(lomadriel, "Ac ne quis a nobis hoc ita dici forte miretur, quod alia quaedam in hoc facultas sit ingeni, neque haec dicendi ratio aut disciplina, ne nos quidem huic uni studio penitus umquam dediti fuimus. Etenim omnes artes, quae ad humanitatem pertinent, habent quoddam commune vinculum, et quasi cognatione quadam inter se continentur.");
     dialogueManager.addDialogue(enerto, "Amor progredi ob habuit amicos contra Coriolano Coriolanus Numne cum num debuerunt ob si ferre ferre si num contra contra amicos si debuerunt debuerunt num patriam Coriolanus amor debuerunt cum videamus debuerunt iuvare si Coriolanus amicos debuerunt videamus debuerunt primum amici Coriolano id regnum primum arma debeat amicos debeat Maelium rem adpetentem debeat ob amici amor patriam in amicos amicitia debuerunt illi in debuerunt Vecellinum Coriolano Coriolanus amicos regnum Numne arma illi primum cum habuit placet in regnum Coriolano illi num num ferre primum num debeat debeat debeat illi regnum illi patriam amicos Maelium ferre in illi iuvare Vecellinum habuit.");
@@ -111,9 +111,9 @@ int main()
         }
 
         window.clear(sf::Color(128, 128, 128, 255));
-        window.draw(mainDialogFrame.getSprite());
-        window.draw(mainDialogFrame.getName());
-        window.draw(mainDialogFrame.getText());
+        window.draw(dialogFrame.getSprite());
+        window.draw(dialogFrame.getName());
+        window.draw(dialogFrame.getText());
 
         window.display();
     }
