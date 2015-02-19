8852ce1671ef55597ebcbbd9c4671488d3a8ee60
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-02-19 13:54:31 +0100

    Removed useless code in main

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -117,17 +117,6 @@ int main()
 
         window.display();
     }
-    cout << "Closed by precedent line" << endl;
-    return 0;
 
-    /*std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
-    for (std::size_t i = 0; i < modes.size(); ++i)
-    {
-        sf::VideoMode mode = modes[i];
-        std::cout << "Mode #" << i << ": "
-                  << mode.width << "x" << mode.height << " - "
-                  << mode.bitsPerPixel << " bpp" << std::endl;
-    }
-    // Create a window with the same pixel depth as the desktop
-    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();*/
+    return 0;
 }
